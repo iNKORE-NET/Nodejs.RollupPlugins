b7ac93c1030cc94631cbf64428e66b91a6e83535
Yoojun Zhou <hereisnotyoojun@outlook.com>
2024-09-08 21:23:31 +0800

    Add eslint & add more info to preserve-comments

--- packages/rollup-plugins/source/plugins/preserve-comments.ts
+++ packages/rollup-plugins/source/plugins/preserve-comments.ts
@@ -1,142 +1,136 @@
 import * as acorn from 'acorn';
-import { walk } from 'estree-walker';
 import { createFilter, FilterPattern } from '@rollup/pluginutils';
 import MagicString from "magic-string";
-import { OutputOptions } from "rollup";
 
-interface PreserveCommentsOptions {
-  shouldPreserve?: boolean | string[] | ((comment: string) => boolean);
-  include?: FilterPattern;
-  exclude?: FilterPattern;
+type CommentPreservingCallback = (comment: acorn.Comment) => boolean;
+
+export const defaultCommentPreservingCallback: CommentPreservingCallback = (comment) =>
+{
+    return comment.value.startsWith("*") || comment.value.startsWith("!") || comment.value.startsWith("@");
 }
 
-export default function preserveComments(options: PreserveCommentsOptions = {}, outputOptions: OutputOptions = {}) 
+export interface PreserveCommentsOptions
 {
-    const 
-    {
+    /**
+     * A boolean means indicating whether to preserve all comments,
+     * a string array means every comment that contains any of the strings will be preserved,
+     * or a function that takes a comment and returns a boolean indicating whether to preserve it.
+     */
+    shouldPreserve?: boolean | string[] | CommentPreservingCallback;
+
+    include?: FilterPattern;
+    exclude?: FilterPattern;
+
+    /**
+     * Modify the sourcemap of the output code to match the changes made to the code.
+     * When there're a large amount of files, it may slow down the build process.
+     * When you don't need sourcemaps, you can set it to false to speed up the build process.
+     * @default true
+     */
+    sourcemap?: boolean;
+}
+
+/**
+ * A Rollup plugin that handles comments in the code, you're in full control of which comments to keep and which to remove.
+ * NOTE: You ALWAYS need to set 'removeComments' to false in the tsconfig when this plugin is used.
+ */
+export default function preserveComments(options: PreserveCommentsOptions = {}) 
+{
+    const {
         shouldPreserve = false,
         include,
         exclude
     } = options;
 
-  return {
-    name: 'preserve-comments',
-    renderChunk(code: string, chunk: any) {
-      // 检查文件后缀名
-      
-        const filter = createFilter(include, exclude);
-              if (!filter(chunk.facadeModuleId)) {
-        return null;
-      }
-    try
-      {
-        if (!chunk.facadeModuleId || !/\.(ts|tsx|js|jsx)$/.test(chunk.facadeModuleId)) {
-        return null; // 不处理非目标文件
-      }
+    return {
+        name: 'preserve-comments',
+        renderChunk(code: string, chunk: any) 
+        {
+            // 检查文件后缀名
 
-      const comments: acorn.Comment[] = [];
-      const ast = acorn.parse(code, {
-        ecmaVersion: "latest",
-        onComment: comments,
-        sourceType: "module",
-      }) as any;
+            const filter = createFilter(include, exclude);
+            if (!filter(chunk.facadeModuleId)) 
+            {
+                this.warn(`Skipping preserve-comments for ${ chunk.facadeModuleId }`);
+                return null;
+            }
 
-      let shouldKeepComment: (comment: string) => boolean;
+            try
+            {
+                const comments: acorn.Comment[] = [];
+                acorn.parse(code,
+                    {
+                        ecmaVersion: "latest",
+                        onComment: comments,
+                        sourceType: "module",
+                    }) as any;
 
-      if (typeof shouldPreserve === 'boolean') 
-      {
-        shouldKeepComment = () => shouldPreserve;
-      } else if (typeof shouldPreserve === 'function') 
-      {
-        shouldKeepComment = shouldPreserve;
-      } else if (Array.isArray(shouldPreserve)) 
-      {
-        const preserveSet = new Set(shouldPreserve.map((item: string) => item.toLowerCase()));
-        shouldKeepComment = (comment) => 
-        {  
-            const commentLower = comment.toLowerCase();
-            return [...preserveSet].some(preserve => commentLower.includes(preserve));
-        }//shouldPreserve.includes(comment);
-      } else {
-        // Default to not preserving comments
-        shouldKeepComment = () => false;
-      }
+                let shouldKeepComment: CommentPreservingCallback = defaultCommentPreservingCallback;
 
-      // Filter out comments based on the shouldPreserve condition
-    //   for (let i = comments.length - 1; i >= 0; i--) {
-    //     const comment = comments[i];
-    //     if (!shouldKeepComment(comment.value)) {
-    //       comments.splice(i, 1);
-    //     }
-    //   }
+                if (typeof shouldPreserve === 'boolean') 
+                {
+                    shouldKeepComment = () => shouldPreserve;
+                }
+                else if (typeof shouldPreserve === 'function') 
+                {
+                    shouldKeepComment = shouldPreserve;
+                }
+                else if (Array.isArray(shouldPreserve)) 
+                {
+                    const preserveSet = new Set(shouldPreserve.map((item: string) => item.toLowerCase()));
+                    shouldKeepComment = (comment) => 
+                    {
+                        const commentLower = comment.value.toLowerCase();
+                        return [...preserveSet].some(preserve => commentLower.includes(preserve));
+                    }
+                }
 
-    //   let newCode = '';
-    //   let lastIndex = 0;
-    //   walk(ast, {
-    //     enter(node: any) {
-    //       if (node.type === 'Program' && comments.length) {
-    //         comments.forEach(comment => {
-    //           const { start, end } = comment;
-    //           newCode += code.slice(lastIndex, start);
-    //           lastIndex = end;
-    //           if (shouldKeepComment(comment.value)) {
-    //             newCode += code.slice(start, end);
-    //           }
-    //         });
-    //         newCode += code.slice(lastIndex);
-    //       }
-    //     }
-    //   });
+                const s = new MagicString(code);
 
-    // 使用MagicString方便地修改源代码
-        const s = new MagicString(code);
+                comments.forEach(comment =>
+                {
+                    if (!shouldKeepComment(comment))
+                    {
+                        let start = comment.start;
+                        // 向前搜索，直到找到非空白字符或字符串起始位置
+                        while (start > 0 && " \t\r\n".includes(code[start - 1]))
+                        {
+                            start--;
+                        }
 
-        // comments.forEach(comment => {
-        // if (!shouldKeepComment(comment.value)) {
-        //     s.remove(comment.start, comment.end);
-        // }
-        // });
-        // const newCode = s.toString();
+                        let end = comment.end;
+                        // 向后搜索，直到找到非空白字符或字符串结束位置
+                        while (end < code.length && " \t\r\n".includes(code[end]))
+                        {
+                            end++;
+                        }
 
-            comments.forEach(comment => {
-        if (!shouldKeepComment(comment.value)) {
-          let start = comment.start;
-          // 向前搜索，直到找到非空白字符或字符串起始位置
-          while (start > 0 && " \t\r\n".includes(code[start - 1])) {
-            start--;
-          }
+                        // 如果此时的end位置是一个换行符，再向后推进一位，以删除整个空行
+                        if (code[end] === '\n')
+                        {
+                            end++;
+                        }
 
-          let end = comment.end;
-          // 向后搜索，直到找到非空白字符或字符串结束位置
-          while (end < code.length && " \t\r\n".includes(code[end])) {
-            end++;
-          }
+                        s.remove(start, end);
+                    }
+                });
 
-          // 如果此时的end位置是一个换行符，再向后推进一位，以删除整个空行
-          if (code[end] === '\n') {
-            end++;
-          }
+                const newCode = s.toString();
+                const newMap = (options.sourcemap ?? true) ? s.generateMap({ hires: true }) : null;
 
-          s.remove(start, end);
+                return {
+                    code: newCode,
+                    map: newMap
+                };
+            }
+            catch (e)
+            {
+                this.error(chunk.facadeModuleId);
+                return {
+                    code: null,
+                    map: null
+                };
+            }
         }
-      });
-
-      const newCode = s.toString();
-      const newMap = outputOptions.sourcemap ? s.generateMap({ hires: true }) : null;
-
-      return {
-        code: newCode || code,
-        map: newMap
-      };
-      }
-      catch(e)
-      {
-        this.error(chunk.facadeModuleId);
-        return {
-        code: null,
-        map: null
-      };
-      }
-    }
-  };
+    };
 }
